7ebca4e389bb607656e2753753d4fdb47d84a164
Hodos Tamas <hodost@gmail.com>
2023-03-06 14:09:32 +0100

    Create "01-Introdution to SELECT Statements"
    Book: Microsoft SQL from A to Z.

---
+++ 01-Introdution to SELECT Statements.sql
@@ -0,0 +1,21 @@
+/**********************************************
+01-Introdution to SELECT Statements
+Microsoft SQL from A to Z
+-----------------------------------------------
+-- VSCode Settings 1. View --> Word Wrap
+-- VSCode Settings 2. View --> Editor Layout --> Flip Layout
+**********************************************/
+
+SELECT 'Hello World'
+SELECT 'Valami', 'Csoda'
+SELECT 'Valami', 'Csoda', 'Bana'
+SELECT 'Adam''s Apple'	-- Use two apostrophes
+SELECT 'John''s Office'
+----------------------------------
+SELECT 4 + 2
+SELECT 4 - 2
+SELECT 4 * 2
+SELECT 4 / 2
+SELECT (5 * 5) - (3 * 5)
+SELECT (5*5)-3+(2*6)
+SELECT 5*(5-3+2)*6
