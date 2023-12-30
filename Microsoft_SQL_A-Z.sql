75173d51420b19781527398c7d563efc300ca00e
Tamas Hodos <hodost@outlook.hu>
2023-12-30 17:02:16 +0100

    Lab 9-Aggregate Functions

--- Microsoft_SQL_A-Z.sql
+++ Microsoft_SQL_A-Z.sql
@@ -445,3 +445,32 @@ FROM HumanResources.Employee E
 INNER JOIN HumanResources.EmployeeDepartmentHistory EDH ON E.BusinessEntityID = EDH.BusinessEntityID;
 
 USE AdventureWorks2019
+
+-- How many rows are in the Person.Person table? Use an aggregate function NOT “SELECT *”.
+SELECT COUNT(*)
+FROM Person.Person
+
+-- How many rows in the Person.Person table do not have a NULL value in the MiddleName column?
+SELECT COUNT(MiddleName)
+FROM Person.Person
+
+-- What is the average StandardCost (located in Production.Product) for each product where the StandardCost is greater than $0.00?
+SELECT AVG(StandardCost)
+FROM Production.Product
+WHERE StandardCost > 0.00
+
+-- What is the average Freight amount for each sale (found in Sales.SalesOrderHeader) where the sale took place in TerritoryID 4?
+SELECT AVG(Freight)
+FROM Sales.SalesOrderHeader
+WHERE TerritoryID = 4
+
+-- How expensive is the most expensive product, by ListPrice, in the table Production.Product?
+SELECT MAX(ListPrice)
+FROM Production.Product
+
+-- Join the Production.Product table and the Production.ProductInventory table for only the products that appear in both table. Use the ProductID as the joining column. Production.ProductInventory contains the quantity of each product (several rows can appear for each product to indicate the product appears in multiple locations). Your goal is to determine how much money we would earn if we sold every product for its list price for each product with a ListPrice greater than $0. That is, if you summed the product of each product’s inventory by its list price, what would that value be?
+SELECT SUM(P.ListPrice * PI.Quantity) AS TotalValue
+FROM Production.Product P
+INNER JOIN Production.ProductInventory PI ON P.ProductID = PI.ProductID
+WHERE P.ListPrice > 0;
+
