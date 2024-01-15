56ab891824cbd9e801853d9e857417cce6e81857
Hodos Tamas <hodost@gmail.com>
2024-01-15 10:09:48 +0100

    Just sync

--- Microsoft_SQL_A-Z.sql
+++ Microsoft_SQL_A-Z.sql
@@ -437,3 +437,5 @@ SELECT
     EDH.DepartmentID, EDH.StartDate, EDH.EndDate
 FROM HumanResources.Employee E 
 INNER JOIN HumanResources.EmployeeDepartmentHistory EDH ON E.BusinessEntityID = EDH.BusinessEntityID;
+
+USE AdventureWorks2019
